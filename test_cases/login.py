5801310ba41245714e215a42e328c94cb7531089
jijdp <138129390+jijdp@users.noreply.github.com>
2024-01-18 20:42:30 +0700

    Update login.py

--- test_cases/login.py
+++ test_cases/login.py
@@ -4,3 +4,12 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
+
+driver.maximize_window()
+
+time.sleep(2)
+
+driver.find_element(By.NAME, "username").send_keys("Admin")
+time.sleep(2)
+driver.find_element(By.NAME,"password").send_keys("admin123")
+time.sleep(2)
